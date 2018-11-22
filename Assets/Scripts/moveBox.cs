41ae02ec5d292ee075db6d9767302b84fd3be430
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2018-11-22 10:19:43 +0100

    startup for p1_app
    this is the starting point for p1_app
    if you want to make something, make a folder with your name and work in
    that folder

---
+++ Assets/Scripts/moveBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBox : MonoBehaviour {
+
+    public int[] xpos = {30,720};
+    public int[] ypos = { -30, -700 };
+    public int speed = 5;
+    int point = 1;
+    public pointerCounter pointcounter;
+    
+    RectTransform rectbox;
+    float currentYPos;
+    Transform boxpos;
+    // Use this for initialization
+    void Start () {
+        currentYPos = ypos[0];
+        rectbox = gameObject.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveTheBox();
+    }
+
+    void moveTheBox()
+    {
+        rectbox.anchoredPosition = new Vector3(xpos[1], currentYPos, 0);
+        if (currentYPos < ypos[1] || currentYPos > ypos[0])
+        {
+            speed *= -1;
+        }
+        if (currentYPos > ypos[0])
+        {
+            pointcounter.addPoint(point);
+        }
+        currentYPos += speed;
+        
+        
+    }
+    
+}
---
+++ Assets/Scripts/pointerCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pointerCounter : MonoBehaviour {
+    public int points = 0;
+    public int numOfAction = 7;
+    public Text textfield;
+
+    void Update()
+    {
+        textfield.text = points +" / "+numOfAction;
+    }
+
+    public void addPoint(int addition)
+    {
+        points++;
+    }
+}
